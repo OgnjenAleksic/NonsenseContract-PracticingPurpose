a1e98e4448559a2eac0f9f0d3d7801ac97bbf097
Ognjen <cryptognjen@gmail.com>
2023-02-01 21:35:56 +0100

    added minimumDeposit

--- contracts/modifier.sol
+++ contracts/modifier.sol
@@ -3,13 +3,20 @@ pragma solidity ^0.8.17;
 
 contract Modifier {
     address payable owner;
+    uint256 private i_minimum;
 
-    constructor() {
+    constructor(uint256 _minimum) {
         owner = payable(msg.sender);
+        i_minimum = _minimum;
     }
 
     modifier onlyOwner() virtual {
         require(msg.sender == owner, "Only owner can withdraw the funds");
+        _;
+    }
+
+    modifier minimumDeposit() virtual {
+        require(msg.value >= i_minimum, "Your deposit is less than minimum");
         _;
     }
 }
