f4527cc910e8648469d8ef29a8c3cd9c74b8a1d6
Ognjen <cryptognjen@gmail.com>
2023-02-01 21:36:59 +0100

    added modifiert

--- contracts/myContract.sol
+++ contracts/myContract.sol
@@ -12,7 +12,7 @@ contract myContract is Modifier {
     address private myAddress = 0xc8338F5BE9c6D9562ab76000f86dC1cd5Fbae0b1;
     uint160 x = uint160(myAddress);
 
-    constructor(uint256 _minimum) {
+    constructor(uint256 _minimum) Modifier(_minimum) {
         i_minimum = _minimum;
     }
 
@@ -23,21 +23,13 @@ contract myContract is Modifier {
         return contractCode;
     }
 
-    function depositUsingSend(address _to) public payable {
-        require(
-            msg.value > i_minimum,
-            "You deposited less than minimum amount"
-        );
+    function depositUsingSend(address _to) public payable minimumDeposit {
         bool sent = payable(_to).send(msg.value);
         if (!sent) revert myContract_EtherNotSent();
         depositors.push(msg.sender);
     }
 
-    function depositUsingCall(address _to) public payable {
-        require(
-            msg.value > i_minimum,
-            "You deposited less than minimum amount"
-        );
+    function depositUsingCall(address _to) public payable minimumDeposit {
         (bool sent /* bytes memory returnData */, ) = payable(_to).call{
             gas: 10000,
             value: msg.value
